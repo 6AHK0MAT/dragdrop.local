39ab11f37bac534c124433f60693dbda1c1762cc
6AHK0MAT <makhmutov.va@mail.ru>
2017-02-16 02:05:01 +0700

    TestWork

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+// Copyright (C) 2016 Sergey Akopkokhyants
+// This project is licensed under the terms of the MIT license.
+// https://github.com/akserg
+
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+
+import { ToastCommunicationService } from './toast/toast-communication.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnDestroy {
+    private sub: any;
+    // Position of Ng2ToastyComponent
+    private toastyComponentPosition: string;
+
+    constructor(private slimLoader: SlimLoadingBarService, private router: Router, 
+                private toastCommunicationService: ToastCommunicationService) {
+        // We listen the position's changes
+        this.toastCommunicationService.position$.subscribe(pos => this.toastyComponentPosition = pos);
+        // Listen the navigation events to start or complete the slim bar loading
+        this.sub = this.router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                this.slimLoader.start();
+            } else if ( event instanceof NavigationEnd ||
+                        event instanceof NavigationCancel ||
+                        event instanceof NavigationError) {
+                this.slimLoader.complete();
+            }
+        }, (error: any) => {
+            this.slimLoader.complete();
+        });
+    }
+
+    ngOnDestroy(): any {
+        this.sub.unsubscribe();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Copyright (C) 2016 Sergey Akopkokhyants
+// This project is licensed under the terms of the MIT license.
+// https://github.com/akserg
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
+import { ToastyModule } from 'ng2-toasty';
+import { DndModule } from 'ng2-dnd';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+import { HomeComponent } from './home/home.component';
+import { SlimComponent } from './slim/slim.component';
+import { DndComponent } from './dnd/dnd.component';
+import { ToastComponent, ToastCommunicationService } from './toast';
+
+import { routing } from './app.routing';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, routing,
+        ToastyModule.forRoot(), DndModule.forRoot(), SlimLoadingBarModule.forRoot()],
+    declarations: [AppComponent, HomeComponent, SlimComponent, DndComponent, ToastComponent],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, ToastCommunicationService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
